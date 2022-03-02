0d5a95c504fffb8af9109855d91f00154dbdc399
Kenny <mccormick84@gmail.com>
2022-03-03 00:28:05 +0900

    프로젝트 생성 / firebase 기본 설정

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/material.dart';
+
+// 파이어베이스 초기화
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+  FirebaseAnalyticsObserver(analytics: analytics);
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorObservers: <NavigatorObserver>[observer],
+      home: FirebaseApp(
+        analytics: analytics,
+        observer: observer,
+      ),
+    );
+  }
+}
+
+class FirebaseApp extends StatefulWidget {
+  FirebaseApp({Key? key, required this.analytics, required this.observer})
+      : super(key: key);
+
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+
+  @override
+  _FirebaseAppState createState() => _FirebaseAppState(analytics, observer);
+}
+
+class _FirebaseAppState extends State<FirebaseApp> {
+  _FirebaseAppState(this.analytics, this.observer);
+
+  final FirebaseAnalyticsObserver observer;
+  final FirebaseAnalytics analytics;
+  String _message = '';
+
+  void setMessages(String message) {
+    setState(() {
+      _message = message;
+    });
+  }
+
+  Future<void> _sendAnalyticsEvent() async {
+    // 애널리틱스의 logEvent를 호출해 test_event라는 키 값으로 데이터 저장
+    await analytics.logEvent(
+      name: 'test_event',
+      parameters: <String, dynamic>{
+        'string': 'hello flutter',
+        'int': 100,
+      },
+    );
+    setMessages('Analytics 보내기 성공');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Example'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: _sendAnalyticsEvent, child: const Text('테스트')),
+            Text(
+              _message,
+              style: const TextStyle(color: Colors.blueAccent),
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.tab),
+        onPressed: () {},
+      ),
+    );
+  }
+}
